9a3d7d09a88ff2fa7f10919de7529a9da426fbdc
tyamzak <getagelessgetageless@gmail.com>
2021-12-09 09:09:44 +0900

    https://github.com/tyamzak/audio_push.git

---
+++ win_talk.py
@@ -0,0 +1,38 @@
+#coding: utf-8
+# call OpenJTalk for windows
+import subprocess
+import winsound
+from datetime import datetime
+
+
+def jtalk(t):
+    # depend on your install folder 
+    OPENJTALK_BINPATH = 'c:/open_jtalk/bin'
+    OPENJTALK_DICPATH = 'c:/open_jtalk/dic'
+    OPENJTALK_VOICEPATH = 'c:/open_jtalk/bin/mei_angry.htsvoice'
+    open_jtalk=[OPENJTALK_BINPATH + '/open_jtalk.exe']
+    mech=['-x',OPENJTALK_DICPATH]
+    htsvoice=['-m',OPENJTALK_VOICEPATH]
+    speed=['-r','0.5']
+    outwav=['-ow','open_jtalk.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+
+    # convert text encoding from utf-8 to shitf-jis
+    c.stdin.write(t.encode('shift-jis'))
+    c.stdin.close()
+    c.wait()
+
+    # play wav audio file with winsound module
+    winsound.PlaySound('futta-dream.wav', winsound.SND_FILENAME)
+
+
+def say_datetime():
+    d = datetime.now()
+    text = '%s月%s日、%s時%s分%s秒' % (d.month, d.day, d.hour, d.minute, d.second)
+    text = input().strip()
+    print(text)
+    jtalk(text)
+
+if __name__ == '__main__':
+    say_datetime()
